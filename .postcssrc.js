cbb6772f0ca6d4e2663351cbb20cbf75e8b9ce56
Yaoshuyi1999 <yaoshuyi19990118@163.com>
2022-07-13 20:00:40 +0800

    准备工作完成

---
+++ .postcssrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+	plugins: {
+		'postcss-pxtorem': {
+			//设计稿1rem的大小
+			// vant组件库是根据37.5
+			rootValue: (module) =>(/vant/gi.test(module.file)? 37.5: 75),
+			//适配的属性
+			propList: ['*']
+		}
+	}
+}
--- src/main.js
+++ src/main.js
@@ -2,14 +2,24 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 引入vant组件
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+// 引入移动适配
+import 'amfe-flexible/index.min.js'
 
 // 加载全局样式
 import './styles/index.less'
+// 导入icon图标样式
+import './assets/fonts/iconfont.css'
 
 Vue.config.productionTip = false
+
+Vue.use(Vant)
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: (h) => h(App)
 }).$mount('#app')
